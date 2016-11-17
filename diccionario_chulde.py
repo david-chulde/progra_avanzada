03f8bf82590ece7b2c46e981b2ede49ff491e845
pc\David <chulded@gmail.com>
2016-11-17 11:15:51 -0500

    diccionario_chulde

---
+++ diccionario_chulde.py
@@ -0,0 +1,13 @@
+diccionario_chulde= {'David': 4098, 'Juan': 4139, 'Danilo': 3356, 'Edison': 2346, 'Michael': 1234}
+diccionario_chulde['Pablo']= 3556
+print(diccionario_chulde)
+print(diccionario_chulde['David'])
+del diccionario_chulde ['David']
+diccionario_chulde['Maria']= 2914
+print(diccionario_chulde)
+print(list(diccionario_chulde.keys()))
+print(sorted(diccionario_chulde.keys()))
+print('Danilo' in diccionario_chulde)
+print('Juan' in diccionario_chulde)
+print('Danilo' not in diccionario_chulde)
+print('Juan' not in diccionario_chulde)
